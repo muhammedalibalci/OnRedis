6a9de662a76a3c41d818ef43fc8f94120e3445dd
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-11 19:24:19 +0300

    Configuration

---
+++ RedisExchange/Models/ProductsList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchange.Models
+{
+    public static class ProductsList
+    {
+        public static List<Product> GenerateProductsList()
+        {
+            List<Product> products = new List<Product>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Product product = new Product();
+                product.Id = i;
+                product.Name = "PC" + i;
+                product.Price = 5000 + i;
+                if (i % 10 == 0)
+                {
+                    product.IsPopular = true;
+                }
+                products.Add(product);
+            };
+
+            return products;
+        }
+    }
+}
