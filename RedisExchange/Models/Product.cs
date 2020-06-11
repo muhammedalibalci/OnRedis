8eab2d84505e8f8b1b4650c5c5aa541bbaddd561
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-11 19:25:49 +0300

    initial commit

---
+++ RedisExchange/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchange.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsPopular { get; set; } = false;
+    }
+}
